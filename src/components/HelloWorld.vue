d9778e6335bb06ff7a8866a783f1f80ab1bef523
Janice-M <janiceink001@gmail.com>
2021-02-02 23:55:44 +0300

    ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,7 +31,15 @@
 </template>
 
 <script>
-
+import { Component, Prop, Vue } from 'vue-property-decorator'
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() readonly msg!: string
+  @Prop({default: 'John doe'}) readonly name: string
+  @Prop({required: true}) readonly age: number
+  @Prop(String) readonly address: string
+  @Prop({required: false, type: String, default: 'Developer'}) readonly job: string
+}
 @Component
 export default class HelloWorld extends Vue {
   private msg: string = "welcome to my app"
